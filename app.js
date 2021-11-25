490555a597f41542b62a479f4efe830f1ad65a18
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-25 03:25:32 -0600

    Update app.js

--- app.js
+++ app.js
@@ -24,10 +24,8 @@ app.use('/themes', themeRouter);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 
 module.exports = app;
